359cb06f1f15efafa39b98895c28a65bfb8e5a67
steven <alvaradosteven315@gmail.com>
2023-01-18 20:16:49 -0500

    verifying if token exists

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,25 +9,33 @@ require('dotenv').config()
 
 const authMiddleware = (req, res, next) => {
   let { authorization: token } = req.headers
-  token = token.replace('Bearer ', '')
+  token = token?.replace('Bearer ', '')
   // const token = req.headers.authorization
   console.log(token)
-  const decoded = jwt.verify(
-    token,
-    process.env.JWT_SECRET,
-    { algorithms: 'HS512' },
-    (err, decoded) => {
-      if (err) {
-        res.status(400).json({
-          error: 'invalid token',
-          message: 'invalid token',
-        })
-      } else {
-        console.log(decoded)
-        next()
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      { algorithms: 'HS512' },
+      (err, decoded) => {
+        if (err) {
+          res.status(400).json({
+            error: 'invalid token',
+            message: 'invalid token',
+          })
+        } else {
+          console.log(decoded)
+          next()
+        }
       }
-    }
-  )
+    )
+  } else {
+    res.status(400).json({
+      error: 'no token provided',
+      message: 'authentication undefined',
+    })
+  }
+  // const decoded =
   // next()
 }
 
